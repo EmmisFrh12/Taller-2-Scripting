5584fb8aad53da48f6a8c918352c66dda0f0a045
MigueMC16 <miguelangel.munoz54@gmail.com>
2024-04-10 12:55:22 -0500

    Archivo funciones
    Añadido archivo de Visual Studio donde crear 30 funciones, 10 cada uno.

---
+++ Funciones/Funciones/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+//Cada uno va a hacer 10 funciones en este archivo para completar las treinta que hacen falta.
+static double Suma (double numero1, double numero2)
+{
+   return numero1 + numero2;
+}
+static double Resta(double numero1, double numero2)
+{
+    return numero1 - numero2;
+}
+static double Producto(double numero1, double numero2)
+{
+    return numero1 * numero2;
+}
+static double Cociente(double numero1, double numero2)
+{
+    if (numero2 == 0)
+    {
+        return 0;
+    }
+    else
+    {
+        return numero1 / numero2;
+    }
+    
+}
